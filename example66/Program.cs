a7b232a69a2a4f7fbb5ba7e89f682c92c44f0188
SergeyMitroshin <sergey@host-best.com>
2022-12-02 23:31:10 +0300

    v02

---
+++ example66/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumElemens (int m, int n)  // с циклом
+{
+    int result = 0;
+        for (int i=m; i<=n; i++) 
+            result+=i;
+    return result;
+}
+
+int SumElemensR(int m,int n)    // с рекурсией
+{
+    int result = 0;
+    if (m<=n)
+        result = m + SumElemensR(m+1,n);
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);    
+// Console.WriteLine("Результат: " + SumElemens(m,n));  // без рекурсии
+Console.WriteLine("Результат: " + SumElemensR(m,n)); // с рекурсией
